b669e43a86714be7238c0bec1290d4eff422469c
kobey <347511974@qq.com>
2020-08-04 19:18:49 +0800

    day1
    Signed-off-by: kobey <347511974@qq.com>

---
+++ 02th_memory_manage/01th_21cnbao/memory-courses/memo.mk
@@ -0,0 +1,12 @@
+#资料准备：
+https://mp.weixin.qq.com/s?__biz=MzAwMDUwNDgxOA==&mid=2652664214&idx=1&sn=4b88af1d6ce95f8222eecae906e1f14d&chksm=810f370bb678be1d1d969683c4bc3abfd130abc3ae8b256357bb041675b76cf567dff270e754&scene=21#wechat_redirect
+
+https://mp.weixin.qq.com/s?__biz=MzAwMDUwNDgxOA==&mid=2652664286&idx=1&sn=370af253ab45e8ce6da7c7eaeae0fbd1&chksm=810f3743b678be55df8f08dd554586e7e6a4e93fe61a9c557f5b161a6242eee6cd463f68a7e9&scene=21#wechat_redirect
+
+课件：
+
+https://mp.weixin.qq.com/s?__biz=MzAwMDUwNDgxOA==&mid=2652663432&idx=1&sn=82c667140eb4398f6a5ad155d16d5674&chksm=810f2815b678a1038ae3ab00fef84ecf071437fad78679db20efd526351487f68295b2778496&scene=21#wechat_redirect
+
+主要笔记帖子：
+https://blog.csdn.net/juS3Ve/article/details/80115557
+
